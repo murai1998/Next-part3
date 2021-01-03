6e25c5f86d5f066b0afc4aabf142d37a53e3f313
Hanna Murai <hannamurai@Hannas-MacBook-Pro.local>
2021-01-03 16:54:12 -0600

    session

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,54 @@
 import Layout from '../components/layout'
-import {  useSession } from 'next-auth/client'
 
-export default function Page (props) {
-  const [ session, loading ] = useSession()
-  console.log('This props', session)
-  return (
+
+// export default function Page (props) {
+//   const [ session, loading ] = useSession()
+//   console.log('This props', session)
+//   return (
     
-    <Layout>
-  { session !== undefined   ? 
-    <>
-     <p>Hi, </p>
-     <p>We are very happy you have jouned us. Welcome!</p>
-     </>
-     : ('')
+//     <Layout>
+//   { session !== undefined   ? 
+//     <>
+//      <p>Hi, </p>
+//      <p>We are very happy you have jouned us. Welcome!</p>
+//      </>
+//      : ('')
+//   }
+//     </Layout>
+    
+//   )
+// }
+
+
+
+import { signIn, signOut, useSession } from 'next-auth/client'
+
+export default function Page() {
+  const [session, loading] = useSession()
+
+  if (loading) {
+    return <p>Loading...</p>
   }
-    </Layout>
-    
+
+  return (
+    <>
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={signIn}>Sign in</button>
+        </>
+      )}
+      {session && (
+       <Layout>
+       { session !== undefined   ? 
+         <>
+          <p>Hi, </p>
+          <p>We are very happy you have jouned us. Welcome!</p>
+          </>
+          : ('')
+       }
+         </Layout>
+      )}
+    </>
   )
 }
