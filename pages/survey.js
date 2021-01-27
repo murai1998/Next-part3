4bf4fae4e9df91fcee373128f26d942b27d2090e
Hanna Murai <hannamurai@Hannas-MacBook-Pro.local>
2021-01-26 21:07:35 -0600

    survey-creator

--- pages/survey.js
+++ pages/survey.js
@@ -25,9 +25,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import nextId from "react-id-generator";
-import { useId } from "react-id-generator";
-import ClearOutlinedIcon from '@material-ui/icons/ClearOutlined';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -55,16 +53,15 @@ const useStyles = makeStyles((theme) => ({
 export default function RecipeReviewCard() {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
-  const [type, setType] = useState('')
   const [questions, setQuestions] = useState([])
-  const [htmlId] = useId();
   const [id, setId] = useState(0)
+  const [double_id, setDouble] = useState(1)
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
   const changeQuestion =(e, id)=>{
-console.log('ID', id)
+
 let all_q = [...questions]
 let index = 0;
 let found = []
@@ -84,6 +81,55 @@ if(found.length > 0){
 }
   }
 
+
+
+  const changeAnswer =(e, id, double_id)=>{
+    let all_q = [...questions]
+    console.log(all_q)
+    let index = 0;
+    let found = []
+    found = all_q.filter((x, i) =>{
+    if(x.id === id){
+        index = i;
+        return x;
+    }
+    })
+    
+    if(found.length > 0){
+
+        let found2 = []
+    let index2 = 0
+        found2 = found[0].answer.map((y, i) =>{
+            if(y.double_v === double_id){
+                index2 = i
+                y.option = e.target.value
+                return y;
+            }
+            else{
+                return y
+            }
+        })
+       
+    console.log('jjjj', found2)
+found[0].answer = found2
+
+        all_q.splice(index, 1, found[0])
+        console.log('FFF', all_q)
+        setTimeout(()=> setQuestions(all_q), 5000)
+    
+    }
+      }
+
+
+  const handleType=(e, id) =>{
+    console.log('E', e.target.value)
+    if(e.target.value === 'one'){
+        
+    }
+  }
+
+
+
   const Question = ()=>{
 return questions.map((x, i)=>{
 
@@ -94,8 +140,8 @@ title={<TextField defaultValue={x.quest}  id="standard-search" onChange={(e)=>ch
 />
 <FormControl>
   <Select
-    defaultValue={x.type}
-    // onChange={handleType}
+    value={x.type}
+    onChange={(e) =>handleType(e, x.id)}
     displayEmpty
     className={classes.selectEmpty}
     inputProps={{ 'aria-label': 'Without label' }}
@@ -105,24 +151,58 @@ title={<TextField defaultValue={x.quest}  id="standard-search" onChange={(e)=>ch
     </MenuItem>
     <MenuItem value='one'>Multiple choices with 1 right answer</MenuItem>
     <MenuItem value='several'>Multiple choices with several anwers</MenuItem>
-
     <MenuItem value='short'>Short Answer</MenuItem>
     <MenuItem value='long'>Long answer</MenuItem>
   </Select>
-  <FormHelperText>Select Type of Response</FormHelperText>
+  {/* <FormHelperText>Select Type of Response</FormHelperText> */}
 </FormControl>
+{
+
+x.answer.map(y =>{
+    if(y) {
+    return < TextField key={y.double_v} defaultValue={y.option}  id="standard-search" onChange={(e)=>changeAnswer(e, x.id, y.double_v)}  type="search" helperText="Option" />  
+    }
+    else{
+        return ''
+    }
+})
+    
+}
+<Button onClick={(e)=>addAnswer(e, x.id)}>Add another option</Button>
 </Card>
 
 })
   }
+
+  const addAnswer =(e, id)=>{
+    let all_q = [...questions];
+    let index = 0;
+    let found = []
+    found = all_q.filter((x, i) =>{
+    if(x.id === id){
+        index = i;
+        return x;
+    }
+    })
+    if(found.length > 0){
+        found[0].answer.push({
+            double_v: double_id,
+            option: ''
+        })
+        all_q.splice(index, 1, found[0])
+    }
+    let new_id = double_id;
+    setDouble(new_id + 1)
+}
+
 
 
   const addQuestion =()=>{
       let all_q = [...questions];
       let new_obj = {
           id: id,
-          quest: '???',
-          type: 'one',
+          quest: '',
+          type: '',
           answer: []
       }
       let new_id = id;
